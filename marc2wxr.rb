8705377680d5a8d87d6bd4f60b3c809454f2ce9b
Mark A. Matienzo <mark@matienzo.org>
2011-06-11 15:47:21 -0400

    Working! Parses a directory of MARCXML files now. Successfully imports into WP w/o errors.

--- marc2wxr.rb
+++ marc2wxr.rb
@@ -72,45 +72,48 @@ def types r
   c << "</ul>\n"
 end
 
-records = MARC::XMLReader.new(ARGV[0])
+xml_files = Dir[File.join(ARGV[0], '*.xml')]
+t = Time.now
 
 wxr = Builder::XmlMarkup.new(:indent => 1)
 wxr.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"
+
+id = 1
 
 wxr.rss 'version' => "2.0", 'xmlns:content' => "http://purl.org/rss/1.0/modules/content/", 'xmlns:wfw' => "http://wellformedweb.org/CommentAPI/", 'xmlns:dc' => "http://purl.org/dc/elements/1.1/", 'xmlns:wp' => "http://wordpress.org/export/1.0/" do
   wxr.channel do
     wxr.title "MARC2WXR"
     wxr.link "http://github.com/anarchivist/marc2wxr"
-    wxr.language "en-us"
-    wxr.ttl "40"
     wxr.description "conversion"
+    wxr.pubDate t.rfc822
+    wxr.language "en"
     wxr.wp :wxr_version, "1.1"
-
-    records.each do |r|
-      wxr.item do
-        wxr.title title(r)
-        wxr.content(:encoded) do
-          wxr.cdata! contents(r)
-        end 
-        wxr.pubDate Time.now.rfc822
-        wxr.guid "http://brooklynhistory.org/library/callno/#{r['099']['a']}", "isPermaLink" => "false"
-        author = "admin"
-        wxr.author author
-        wxr.dc :creator, author
-        # for category in a.categories
-        #           wxr.category category.name
-        #         end
-        #         for tag in a.tags
-        #           wxr.category tag.display_name
-        #         end
-        # wxr.wp :post_id, a.id
-        wxr.wp :post_date, Time.now.rfc822
-        # wxr.wp :comment_status, 'closed'
-        # wxr.wp :ping_status, 'closed'
-        # wxr.wp :post_name, a.permalink
-        wxr.wp :status, 'publish'
-        wxr.wp :post_parent, '0'
-        wxr.wp :post_type, 'post'
+    wxr.wp :base_site_url, "http://brooklynhistory.org/library/wp"
+    wxr.wp :base_blog_url, "http://brooklynhistory.org/library/wp"
+    
+    xml_files.each do |x|      
+      MARC::XMLReader.new(x).each do |r|
+        wxr.item do
+          wxr.title title(r)
+          wxr.link "http://brooklynhistory.org/library/callno/#{r['099']['a']}"
+          wxr.pubDate t.rfc822
+          wxr.dc :creator, "admin"
+          wxr.guid "http://brooklynhistory.org/library/callno/#{r['099']['a']}", "isPermaLink" => "false"
+          wxr.description ""
+          wxr.content(:encoded) do
+            wxr.cdata! contents(r)
+          end
+          wxr.wp :post_id, id.to_s
+          wxr.wp :post_date, t.strftime('%Y-%m-%d %H:%M:%S')
+          wxr.wp :post_date_gmt, t.utc.strftime('%Y-%m-%d %H:%M:%S')
+          wxr.wp :comment_status, 'closed'
+          wxr.wp :ping_status, 'open'
+          wxr.wp :status, 'publish'
+          wxr.wp :post_parent, '0'
+          wxr.wp :post_type, 'post'
+          wxr.wp :is_sticky, '0'
+        end
+      id += 1
       end
     end
   end
